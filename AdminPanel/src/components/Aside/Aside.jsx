14399d474121f0c94e494de9fa166b4c2ea368d9
priyaLakhani <raxitbambharoliya@gmail.com>
2024-04-09 08:17:11 +0530

    jquery

--- AdminPanel/src/components/Aside/Aside.jsx
+++ AdminPanel/src/components/Aside/Aside.jsx
@@ -1,34 +1,56 @@
-import React, { useEffect, useState } from 'react'
-import { useSelector ,useDispatch} from 'react-redux'
-import { toggleMenu } from '../../store/themSlice';
+import React, { useEffect } from 'react'
+import $ from 'jquery'
+import Logo from '../Logo/Logo'
+import { Link } from 'react-router-dom';
 export default function Aside() {
-    const [className, setClassName]=useState()
-    const them = useSelector((state) => state);
-    const dispatch = useDispatch();
+
+    const toggleMenu = () => {
+        const aside = document.getElementById("aside");
+        //   console.log('aside', aside)
+        aside.classList.toggle('hide');
+    }
     useEffect(() => {
-        console.log('them TTT ', them)
-        if (them.showMenu) {
-            setClassName('show')
-        } else {
-            setClassName('hide')
-        }
-    }, [them])
 
-
+        $('.sideMenu').on('click', function () {
+            $(this).siblings().children('.subMenuList').slideUp();
+            $(this).children('.subMenuList').slideToggle();
+        })
+    }, [])
 
     return (
+        <>
+            <aside id='aside  '   >
+                <div className="closeIcon iconButton position-absolute top-0 end-0 mt-2" onClick={toggleMenu}>
+                    <i className="fa-solid fa-xmark" />
+                </div>
+                <nav>
+                    <div className='asideLogo text-lg-center d-lg-none  ms-2 ms-lg-0 my-3'>
+                        <Logo />
+                    </div>
+                    <ul className='sideMenuList p-0 mt-lg-4'>
+                        <li className='sideMenu' >
+                        
+                            <Link>
+                                Admin
+                            </Link>
+                            <ul className='subMenuList '>
+                                <li className='subMenu'>test 1 </li>
+                                <li className='subMenu'>test 2 </li>
+                                <li className='subMenu'>test 3 </li>
+                            </ul>
+                        </li>
+                        <li className='sideMenu'>
+                            test
+                            <ul className='subMenuList '>
+                                <li className='subMenu'>test 1 </li>
+                                <li className='subMenu'>test 2 </li>
+                                <li className='subMenu'>test 3 </li>
+                            </ul>
+                        </li>
+                    </ul>
+                </nav>
+            </aside>
 
-        <aside className={className} >
-            <div className="closeIcon iconButton position-absolute top-0 end-0 mt-2" onClick={(e) => { dispatch(toggleMenu(!them.showMenu)) }}>
-                <i className="fa-solid fa-xmark" />
-            </div>
-            <nav>
-                <ul>
-                    <li>
-                        test
-                    </li>
-                </ul>
-            </nav>
-        </aside>
+        </>
     )
 }
--- AdminPanel/src/components/Header/Header.jsx
+++ AdminPanel/src/components/Header/Header.jsx
@@ -2,31 +2,20 @@ import React, { useEffect, useState } from 'react'
 import Logo from '../Logo/Logo'
 import '../../assets/css/style.css'
 import '../../assets/css/header.css'
-import { useDispatch, useSelector } from 'react-redux';
-import { toggleMenu } from '../../store/themSlice';
 export default function Header() {
-  const dispatch = useDispatch();
-  let them = useSelector((state) => state);
-  const [showMenu, setShowMenu] = useState(them.showMenu);
 
-  useEffect(() => {
-    if (window.innerWidth < 992) {
-      dispatch(toggleMenu(false))
-      setShowMenu(false);
-    } else {
-      dispatch(toggleMenu(true))
-      setShowMenu(true);
-    }
-  }, [window.innerWidth])
-
+  const toggleMenu = () => {
+    const aside = document.getElementById("aside");
+    aside.classList.toggle('hide');
+  }
   return (
     <header >
       <div className="d-flex align-items-center justify-content-between">
         <div className="logo d-flex align-items-center">
-          {!showMenu && (
-            <div className="toggleButton iconButton " onClick={(e) => { dispatch(toggleMenu(!showMenu)) }}>
-              <i className="fa-solid fa-bars" />
-            </div>)}
+
+          <div className="toggleButton iconButton " onClick={toggleMenu}>
+            <i className="fa-solid fa-bars" />
+          </div>
           <Logo />
         </div>
         <div className="userProfile d-flex  align-items-center">
